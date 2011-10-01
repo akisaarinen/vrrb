3bdcc32f487f833a56ed686228ab00325cbb79f3
Aki Saarinen <aki@akisaarinen.fi>
2011-10-01 15:58:18 +0300

    Remove old views

--- vr.rb
+++ vr.rb
@@ -11,13 +11,11 @@ require 'json'
 require './parser.rb'
 require 'leg_info_finder'
 
-$config = YAML.load_file("vr.yml")
 vr_parser = VrParser.new
 leg_info_finder = LegInfoFinder.new
 
 get '/' do
-  @routes = $config.keys
-  erb :show_all 
+  redirect '/find'
 end
 
 get '/api/station/:station.json' do
@@ -44,49 +42,22 @@ end
 get '/showtrains' do
   @from = leg_info_finder.trains.find_station_by_name(params[:from])
   @to = leg_info_finder.trains.find_station_by_name(params[:to])
-  @trains = leg_info_finder.realtime_trains_for_leg(@from, @to)
-  erb :showtrains
-end
 
-get '/trains/:route' do
-  route_config = $config[params[:route]]
-  
-  @select_by_target = (route_config["select_by_target"] == true)
-  @reference_station_code = route_config["reference_station_code"]
-  @local_station = route_config["local_station"]
-  @target_station = route_config["target_station"]
-  @source_station = route_config["source_station"]
-
-  def train_selector(select_by_target, target_station, source_station, source, target)
-    if select_by_target
-      target == target_station
-    else
-      source == source_station
-    end
-  end
-
-  trains = vr_parser.fetch_all_trains(@reference_station_code)
-  selected_trains = trains.select { |t|
-    train_selector(@select_by_target, @target_station, @source_station, t["stations"].first["name"], t["target"])
+  realtime_train_list = leg_info_finder.realtime_trains_for_leg(@from, @to)
+  realtime_train_details = realtime_train_list.map { |t|
+    vr_parser.fetch_single_train(t["id"])
   }
 
-  @trains = selected_trains.map { |t|
+  @trains = realtime_train_details.map { |t|
     name = t["name"]
     url = t["url"]
     update_info = t["update_time"]
     target = t["target"]
     stations = t["stations"]
-
     last_station = stations.reverse.find { |s| s['dep_actual'] != nil && s["dep_actual"] != "" } || stations.first
-    puts "name: " + name
-    kilo = stations.find { |s| s['name'] == @local_station }
-    if (kilo == nil) 
-      nil 
-    else
-      [name, url, update_info, target, last_station, kilo]
-    end
+    from_station = stations.find { |s| s['name'] == @from.name }
+    [name, url, update_info, target, last_station, from_station]
   }
-  @trains = @trains.select { |x| x != nil }
   erb :show_single
 end
 
