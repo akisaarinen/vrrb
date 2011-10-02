b26f1dd45f6ed624043cdd24e9a826d43742f105
Aki Saarinen <aki@akisaarinen.fi>
2011-10-02 21:36:39 +0300

    Use divs in ajax search and add more test data

--- public/vrrb.js
+++ public/vrrb.js
@@ -36,7 +36,7 @@ function train_lastKnownStation(train) {
     if (typeof(lastKnownStation) != "undefined") {
         return lastKnownStation
     } else {
-        return _(train.stations).first
+        return null
     }
 }
 
@@ -65,8 +65,8 @@ function ui_showSearchResults(realTimeResultCount) {
 function ui_addTrainToSearchResults(train) {
     $("#train-list").append(
         "<li class=\"train_" + train.id + "\">" +
-            "<span class=\"name\">" + train.name + "</span>"+
-            "<span class=\"loading-details\">(haetaan tarkempia tietoja)</span>" +
+            "<div class=\"name\">" + train.name + "</div>"+
+            "<div class=\"loading-details\">(haetaan tarkempia tietoja)</div>" +
         "</li>")
 }
 
@@ -74,7 +74,11 @@ function ui_addTrainDetailsToSearchResults(train) {
     ui_hideLoadingDetailsForTrain(train.id)
     var elem = $("#train-list .train_" + train.id)
     var lastKnownStation = train_lastKnownStation(train)
-    elem.append("<span class=\"last-known-station\">" + JSON.stringify(lastKnownStation) + "</span>")
+    var text = "Ei tietoja"
+    if (lastKnownStation) {
+        text = "Viimeksi @" + lastKnownStation.name + " kello " + lastKnownStation.actual_departure
+    }
+    elem.append("<div class=\"last-known-station\">" + text + "</div>")
 }
 
 function ui_onSearchClick() {
@@ -131,9 +135,15 @@ function toFieldValue() {
 }
 
 function doTest() {
-    var testTrain = {"update_time":"2.10.2011, klo 21:01.","stations":[{"actual_departure":null,"scheduled_arrival":null,"actual_arrival":null,"name":"Vantaankoski","code":null,"scheduled_departure":"20:41"},{"actual_departure":null,"scheduled_arrival":"20:42","actual_arrival":null,"name":"Martinlaakso","code":null,"scheduled_departure":"20:42"},{"actual_departure":null,"scheduled_arrival":"20:44","actual_arrival":null,"name":"Louhela","code":null,"scheduled_departure":"20:44"},{"actual_departure":null,"scheduled_arrival":"20:46","actual_arrival":null,"name":"Myyrmäki","code":null,"scheduled_departure":"20:46"},{"actual_departure":null,"scheduled_arrival":"20:48","actual_arrival":null,"name":"Malminkartano","code":null,"scheduled_departure":"20:48"},{"actual_departure":null,"scheduled_arrival":"20:50","actual_arrival":null,"name":"Kannelmäki","code":null,"scheduled_departure":"20:50"},{"actual_departure":null,"scheduled_arrival":"20:52","actual_arrival":null,"name":"Pohjois-Haaga","code":null,"scheduled_departure":"20:52"},{"actual_departure":"20:54","scheduled_arrival":"20:54","actual_arrival":"20:54","name":"Huopalahti","code":null,"scheduled_departure":"20:54"},{"actual_departure":null,"scheduled_arrival":"20:56","actual_arrival":null,"name":"Ilmala","code":null,"scheduled_departure":"20:56"},{"actual_departure":null,"scheduled_arrival":"20:58","actual_arrival":null,"name":"Pasila","code":null,"scheduled_departure":"20:58"},{"actual_departure":"21:03","scheduled_arrival":"21:03","actual_arrival":"21:03","name":"Helsinki","code":null,"scheduled_departure":null}],"name":"M","url":"http://ext-service.vr.fi/juku/juna.action?lang=fi&junalaji=ll&junanro=8968","id":"8968"}
-    ui_showSearchResults(1)
-    ui_addTrainToSearchResults(testTrain)
-    ui_addTrainDetailsToSearchResults(testTrain)
+    var testTrains = [
+        {"update_time":"2.10.2011, klo 21:01.","stations":[{"actual_departure":null,"scheduled_arrival":null,"actual_arrival":null,"name":"Vantaankoski","code":null,"scheduled_departure":"20:41"},{"actual_departure":null,"scheduled_arrival":"20:42","actual_arrival":null,"name":"Martinlaakso","code":null,"scheduled_departure":"20:42"},{"actual_departure":null,"scheduled_arrival":"20:44","actual_arrival":null,"name":"Louhela","code":null,"scheduled_departure":"20:44"},{"actual_departure":null,"scheduled_arrival":"20:46","actual_arrival":null,"name":"Myyrmäki","code":null,"scheduled_departure":"20:46"},{"actual_departure":null,"scheduled_arrival":"20:48","actual_arrival":null,"name":"Malminkartano","code":null,"scheduled_departure":"20:48"},{"actual_departure":null,"scheduled_arrival":"20:50","actual_arrival":null,"name":"Kannelmäki","code":null,"scheduled_departure":"20:50"},{"actual_departure":null,"scheduled_arrival":"20:52","actual_arrival":null,"name":"Pohjois-Haaga","code":null,"scheduled_departure":"20:52"},{"actual_departure":"20:54","scheduled_arrival":"20:54","actual_arrival":"20:54","name":"Huopalahti","code":null,"scheduled_departure":"20:54"},{"actual_departure":null,"scheduled_arrival":"20:56","actual_arrival":null,"name":"Ilmala","code":null,"scheduled_departure":"20:56"},{"actual_departure":null,"scheduled_arrival":"20:58","actual_arrival":null,"name":"Pasila","code":null,"scheduled_departure":"20:58"},{"actual_departure":"21:03","scheduled_arrival":"21:03","actual_arrival":"21:03","name":"Helsinki","code":null,"scheduled_departure":null}],"name":"M","url":"http://ext-service.vr.fi/juku/juna.action?lang=fi&junalaji=ll&junanro=8968","id":"8968"},
+        {"update_time":"2.10.2011, klo 21:16.","stations":[{"actual_departure":"21:07","scheduled_arrival":null,"actual_arrival":null,"name":"Helsinki","code":null,"scheduled_departure":"21:07"},{"actual_departure":null,"scheduled_arrival":"21:12","actual_arrival":null,"name":"Pasila","code":null,"scheduled_departure":"21:12"},{"actual_departure":"21:15","scheduled_arrival":"21:15","actual_arrival":"21:15","name":"Huopalahti","code":null,"scheduled_departure":"21:15"},{"actual_departure":null,"scheduled_arrival":"21:19","actual_arrival":null,"name":"Leppävaara","code":null,"scheduled_departure":"21:19"},{"actual_departure":null,"scheduled_arrival":"21:21","actual_arrival":null,"name":"Kilo","code":null,"scheduled_departure":"21:21"},{"actual_departure":null,"scheduled_arrival":"21:23","actual_arrival":null,"name":"Kera","code":null,"scheduled_departure":"21:23"},{"actual_departure":null,"scheduled_arrival":"21:26","actual_arrival":null,"name":"Kauniainen","code":null,"scheduled_departure":"21:26"},{"actual_departure":null,"scheduled_arrival":"21:28","actual_arrival":null,"name":"Koivuhovi","code":null,"scheduled_departure":"21:28"},{"actual_departure":null,"scheduled_arrival":"21:30","actual_arrival":null,"name":"Tuomarila","code":null,"scheduled_departure":"21:30"},{"actual_departure":null,"scheduled_arrival":"21:32","actual_arrival":null,"name":"Espoo","code":null,"scheduled_departure":"21:32"},{"actual_departure":null,"scheduled_arrival":"21:35","actual_arrival":null,"name":"Kauklahti","code":null,"scheduled_departure":"21:35"},{"actual_departure":null,"scheduled_arrival":"21:40","actual_arrival":null,"name":"Masala","code":null,"scheduled_departure":"21:40"},{"actual_departure":null,"scheduled_arrival":"21:47","actual_arrival":null,"name":"Kirkkonummi","code":null,"scheduled_departure":null}],"name":"S","url":"http://ext-service.vr.fi/juku/juna.action?lang=fi&junalaji=ll&junanro=8573","id":"8573"},
+        {"update_time":"2.10.2011, klo 0:03.","stations":[{"actual_departure":null,"scheduled_arrival":null,"actual_arrival":null,"name":"Vantaankoski","code":null,"scheduled_departure":"21:41"},{"actual_departure":null,"scheduled_arrival":"21:42","actual_arrival":null,"name":"Martinlaakso","code":null,"scheduled_departure":"21:42"},{"actual_departure":null,"scheduled_arrival":"21:44","actual_arrival":null,"name":"Louhela","code":null,"scheduled_departure":"21:44"},{"actual_departure":null,"scheduled_arrival":"21:46","actual_arrival":null,"name":"Myyrmäki","code":null,"scheduled_departure":"21:46"},{"actual_departure":null,"scheduled_arrival":"21:48","actual_arrival":null,"name":"Malminkartano","code":null,"scheduled_departure":"21:48"},{"actual_departure":null,"scheduled_arrival":"21:50","actual_arrival":null,"name":"Kannelmäki","code":null,"scheduled_departure":"21:50"},{"actual_departure":null,"scheduled_arrival":"21:52","actual_arrival":null,"name":"Pohjois-Haaga","code":null,"scheduled_departure":"21:52"},{"actual_departure":null,"scheduled_arrival":"21:54","actual_arrival":null,"name":"Huopalahti","code":null,"scheduled_departure":"21:54"},{"actual_departure":null,"scheduled_arrival":"21:56","actual_arrival":null,"name":"Ilmala","code":null,"scheduled_departure":"21:56"},{"actual_departure":null,"scheduled_arrival":"21:58","actual_arrival":null,"name":"Pasila","code":null,"scheduled_departure":"21:58"},{"actual_departure":null,"scheduled_arrival":"22:03","actual_arrival":null,"name":"Helsinki","code":null,"scheduled_departure":null}],"name":"M","url":"http://ext-service.vr.fi/juku/juna.action?lang=fi&junalaji=ll&junanro=8986","id":"8986"}
+        ]
+    ui_showSearchResults(testTrains.length)
+    _(testTrains).each(function(t) {
+        ui_addTrainToSearchResults(t)
+        ui_addTrainDetailsToSearchResults(t)
+    })
 }
 
