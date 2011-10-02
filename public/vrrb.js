362aabf0a7fc25b3d83cc903867b13fc7211ef72
Aki Saarinen <aki@akisaarinen.fi>
2011-10-02 20:57:17 +0300

    Cleanup javascript

--- public/vrrb.js
+++ public/vrrb.js
@@ -1,47 +1,13 @@
-function fetchStations(callback) {
+function api_stations(callback) {
     $.get("/api/stations.json", callback)
 }
 
-function fetchTrains(from, to, callback) {
+function api_trains(from, to, callback) {
     $.get("/api/trains/search.json?from=" + from + "&to=" + to, callback)
 }
 
-function fetchSingleTrain(train, callback) {
+function api_train_details(train, callback) {
     $.get("/api/train/" + train.id + ".json", callback)
-}
-
-function fetchTrainList(station) {
-    function handleSingleTrain(t, status) {
-        var selector = '#trainList .train_' + t.id
-        $(selector + ' img.loader').hide();
-        $(selector).append('(' + t.source + " - " + t.target + '), päivitetty ' + t.update_time)
-    }
-
-    function handleTrainList(trainList, status) {
-        $('#loading').hide()
-        $('#trainList .trainCount').html(trainList.length)
-
-        for (var i = 0; i < trainList.length; i++) {
-            var t = trainList[i]
-            var jsonUrl = '/api/train/' + t.id + '.json'
-            $('#trainList .trains').append('<li class="train_' + t.id + '"><a href="' + jsonUrl + '">' + t.name + ' (' + t.id + ') <img class="loader" src="/ajax-loader.gif"></a></li>')
-            $.get(jsonUrl, handleSingleTrain)
-        }
-
-        $('#trainList .jsonContent').append("<pre>"+JSON.stringify(trainList)+"</pre>")
-        $('#trainList').show()
-    }
-
-    $.get("/api/station/" + station + ".json", handleTrainList)
-}
-
-function searchClick() {
-    var from = $('#from option:selected').text()
-    var to = $('#to option:selected').text()
-    if (from != "" && to != "") {
-        ui_showLoading()
-        doAjaxSearch(from, to)
-    }
 }
 
 function handleSingleTrainResult(train) {
@@ -56,24 +22,18 @@ function handleTrainSearch(trains) {
     ui_setSearchResultCount(trains.length)
     _(trains).each(function(train) {
         ui_addTrainToSearchResults(train)
-        fetchSingleTrain(train, handleSingleTrainResult)
+        api_train_details(train, handleSingleTrainResult)
     })
     ui_showSearchResults()
 }
-
-
-function doAjaxSearch(from, to) {
-    fetchTrains(from, to, handleTrainSearch)
-}
-
 
 function train_lastKnownStation(train) {
     console.log(JSON.stringify(train.stations))
     var lastKnownStation = _(train.stations).chain().reverse().detect(function(s) {
         console.log("s="+ JSON.stringify(s))
         s.actual_departure != null
-    })
-    if (lastKnownStation) {
+    }).value()
+    if (typeof(lastKnownStation) != "undefined") {
         return lastKnownStation
     } else {
         return _(train.stations).first
@@ -116,12 +76,20 @@ function ui_addTrainToSearchResults(train) {
 function ui_addTrainDetailsToSearchResults(train) {
     var elem = $("#train-list .train_" + train.id)
     var lastKnownStation = train_lastKnownStation(train)
-    elem.append("<span class=\"last-known-station>" + JSON.stringify(lastKnownStation) + "</span>")
-    elem.append('kulli')
+    elem.append("<span class=\"last-known-station\">" + JSON.stringify(lastKnownStation) + "</span>")
+}
+
+function ui_onSearchClick() {
+    var from = $('#from option:selected').text()
+    var to = $('#to option:selected').text()
+    if (from != "" && to != "") {
+        ui_showLoading()
+        api_trains(from, to, handleTrainSearch)
+    }
 }
 
 $(document).ready(function() {
-    fetchStations(function(stations) {
+    api_stations(function(stations) {
         _(stations).each(function (s) {
             var newOption = "<option>"+s.name+"</option>"
             $("#from").append(newOption)
