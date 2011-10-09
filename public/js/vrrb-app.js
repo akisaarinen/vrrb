9e79e0778c8f1120a0fb041c5b34c90422d34cad
Aki Saarinen <aki@akisaarinen.fi>
2011-10-09 21:19:42 +0300

    Trigger searches in backbone.js application

--- public/js/vrrb-app.js
+++ public/js/vrrb-app.js
@@ -22,18 +22,60 @@ $(document).ready(function() {
         url: "/api/stations.json"
     });
 
+    app.model.StationSelection = Backbone.Model.extend({
+        defaults: {
+            selected: undefined
+        }
+    })
+
+    app.model.TrainSearchResult = Backbone.Model.extend({
+    });
+    app.model.TrainSearchResults = Backbone.Collection.extend({
+        model: app.model.TrainSearchResult
+    });
+    app.model.TrainSearch = Backbone.Model.extend({
+        initialize: function() {
+            _.bindAll(this);
+            this.isSearching = false;
+            this.set({
+                results: new app.model.TrainSearchResults
+            });
+            this.setCollectionUrl();
+            this.bind("change:to", this.setCollectionUrl);
+            this.bind("change:from", this.setCollectionUrl);
+            this.get("results").bind("reset", this.resultsDone);
+        },
+        search: function() {
+            console.log("Starting search to url " + this.get("results").url);
+            this.isSearching = true;
+            this.trigger("change:results", this.get("results"));
+            this.get("results").fetch();
+        },
+        setCollectionUrl: function() {
+            this.get("results").url = "/api/trains/search.json?from=" + this.get("from") + "&to=" + this.get("to");
+            console.log("Updated search url to " + this.get("results").url);
+        },
+        resultsDone: function(x) {
+            this.isSearching = false;
+            this.trigger("change:results", x);
+        },
+        isValidSearch: function() {
+            return typeof(this.get("from")) != "undefined" &&
+                typeof(this.get("to")) != "undefined"
+        }
+    });
+
     app.view.StationOption = Backbone.View.extend({
         tagName: "option",
         render: function() {
             $(this.el).html(this.model.get("name"));
         }
     })
-
     app.view.StationSelect = Backbone.View.extend({
         initialize: function() {
             _.bindAll(this);
-            this.model.bind("add", this.stationAdded);
-            this.model.bind("reset", this.stationsReset);
+            this.model.get("stations").bind("add", this.stationAdded);
+            this.model.get("stations").bind("reset", this.stationsReset);
         },
         events: {
             "change": "onChange"
@@ -51,8 +93,35 @@ $(document).ready(function() {
             view.render();
             $(this.el).append(view.el);
         },
-        onChange: function() {
-            console.log("Change in selection");
+        onChange: function(x) {
+            var selectedText = this.$("option:selected").text();
+            var selectedStation = this.model.get("stations").find(function(s) {
+                return s.get("name") == selectedText
+            });
+            this.model.set({
+                selected: selectedStation
+            });
+        }
+    });
+
+    app.view.TrainSearchResults = Backbone.View.extend({
+        initialize: function() {
+            _.bindAll(this);
+            this.model.bind("change:results", this.resultsChange);
+        },
+        resultsChange: function(model) {
+            console.log("results change, model: " + JSON.stringify(model));
+            this.render();
+        },
+        render: function() {
+            if (this.model.isValidSearch() && !this.model.isSearching) {
+                console.log("isSearching: " + this.model.isSearching);
+                var results = this.model.get("results");
+                $(this.el).show();
+                this.$("#realtime-result-count").html(results.size());
+            } else {
+                $(this.el).hide();
+            }
         }
     });
 
@@ -60,15 +129,45 @@ $(document).ready(function() {
         el: $("#app"),
         initialize: function() {
             var stations = new app.model.Stations();
-            var fromSelect = new app.view.StationSelect({
-                el: $("#from"),
-                model: stations
+
+            var fromSelection = new app.model.StationSelection({
+                stations: stations
             });
-            var toSelect = new app.view.StationSelect({
+            var toSelection = new app.model.StationSelection({
+                stations: stations
+            });
+            var trainSearch = new app.model.TrainSearch();
+
+            fromSelection.bind("change:selected", function(selection) {
+                trainSearch.set({
+                    from: selection.get("selected").get("name")
+                });
+                if (trainSearch.isValidSearch()) {
+                    trainSearch.search();
+                }
+            });
+            toSelection.bind("change:selected", function(selection) {
+                trainSearch.set({
+                    to: selection.get("selected").get("name")
+                });
+                if (trainSearch.isValidSearch()) {
+                    trainSearch.search();
+                }
+            });
+            new app.view.StationSelect({
+               el: $("#from"),
+               model: fromSelection
+            });
+            new app.view.StationSelect({
                 el: $("#to"),
-                model: stations
+                model: toSelection
             });
+            new app.view.TrainSearchResults({
+                el: $("#search-results"),
+                model: trainSearch
+            })
             stations.fetch();
+
         }
     });
 
